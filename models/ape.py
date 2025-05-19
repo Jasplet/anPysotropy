a6b829b81cc28bda03dc76bfbabe9073903c0072
Joseph Asplet <joseph.asplet@earth.ox.ac.uk>
2025-05-19 17:17:02 +0100

    added local imports

--- models/ape.py
+++ models/ape.py
@@ -1,6 +1,6 @@
 import numpy as np
-
-
+from anPysotropy.utils.matrix_tools import mkiso, mk66
+from anPysotropy.models.hudson import hudson_c_real
 
 
 def drained_r(psi, theta, s1, s2, s3, p, r0, crit):
@@ -30,3 +30,68 @@ def drained_r(psi, theta, s1, s2, s3, p, r0, crit):
     r[r < 0] = 0.0
 
     return r
+
+
+
+def drained_ape(s1,
+                s2,
+                s3,
+                p,
+                material,
+                npsi=100,
+                ntheta=200):
+    '''
+    Calculates an anisotropic APE tensor (6x6 Voight notiation form) for drained
+    cracks in a rock matrix. Hudson (1981)'s model is
+    used for crack anisotropy.
+    '''
+    psi = np.linspace(0, np.pi, npsi)
+    theta = np.linspace(0, 2.0*np.pi, ntheta)
+
+    dp = psi[1]-psi[0]
+
+    dt = theta[1]-theta[0]
+
+    # make the background isotropic tensor
+    C0 = ap.mkiso(material['lam'],
+                  material['mu'])
+    # convert of 6x6 Voight notation
+    M0 = ap.mk66(C0)
+
+    # calculate the aspect ratio distribution
+    g = ap.drained_r(psi,
+                     theta,
+                     s1,
+                     s2,
+                     s3,
+                     p,
+                     material['r0'],
+                     material['crit'])
+    # calculate the anisotropic elastic tensor
+    # calc base hudson tensor outside of loops
+    # is it does not depend on anything inside.
+    H_base = hudson_c_real(material['lam'],
+                           material['mu'],
+                           material['eps'],
+                           material['r'],
+material['kf'])
+    for i in range(0, npsi):
+        for j in range(0, ntheta):
+            pp = psi[i]
+            tt = theta[j]
+            r = g[i, j]
+            if np.isclose(r, 0):
+                pass
+            else:
+                R = ap.rot_m(-1.0*tt, -1.0*pp)
+                r = r * material['r0']
+                # copy H_base so we can modify it without changing the original
+                H = H_base.copy()
+                dH = (H - M0)*dt*dp*np.sin(pp)/(4.0*np.pi)
+                dH4 = ap.mk3333(dH)
+                dH4r = ap.Rot3333(dH4, R)
+                C0 = C0 + dH4r
+
+    M = ap.mk66(C0)
+
+    return M
